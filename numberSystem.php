5e0f8a4cdc51bdf5e4241a92cd82c0e562f960b1
Shuvo793 <cse.engr.shuvo@gmail.com>
2019-05-01 01:47:01 +0600

    practice started

---
+++ numberSystem.php
@@ -0,0 +1,11 @@
+<?php
+$n=123; //decimal number
+$o=012; // 0 theke suru korle shaita octal (10 hocce 8) (11 hocce 9 )
+$h=0x2B;//hexa decimal number show korar jonno ai format
+//printf("the numbser is %d and %d and %d",$n,$o,$h);
+$b=122; //binary format ee convert korte hobe and binary jonno %b
+        //hexdimal format ee convert korte hole %x;
+        //octal format ee convert korte hole %o
+//printf("the binary equivalent of %d id %b",$b,$b);
+//printf("the hexadecimal equivalent of %d is %o",$b,$b);
+printf("octal %o = %d",0200000,0200000);//octal theke binary result

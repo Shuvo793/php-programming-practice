e7624f6d66d02c7c71225472084a8210b6eededc
Shuvo793 <cse.engr.shuvo@gmail.com>
2019-05-03 21:13:30 +0600

    operator_presidence

---
+++ operator_presidence.php
@@ -0,0 +1,9 @@
+<?php
+
+$a = false || true;// (||) ai operator er somoe asign haoar age run hoe 
+//output=true
+$b = false or true;// (or) ai operator er somoe asign haoar por run hoe ....asign haoar karone $b te (false) asign hoe jae
+//output =false
+var_dump($a,$b);
+//sokol logical operator ex:(or,and, ||,&&) er belae logic same hoe 
+

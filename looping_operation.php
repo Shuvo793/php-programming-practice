2048f1fe79d22b554321cab956e085ae29eeba1c
Shuvo793 <cse.engr.shuvo@gmail.com>
2019-05-04 02:26:24 +0600

    smart conding and dump coding

--- looping_operation.php
+++ looping_operation.php
@@ -7,6 +7,27 @@ $factorial = 1;
     } 
 printf("Factorial number is %d = %d",$number,$factorial);
 echo "\n";
+//dam coding 1 to 10 factorial number
+$j = 1; //j ta dia shudhu operation hoice koeta ta cheak korlam aita factorial program er kono ongsho na
+for ($n=1;$n<=10;$n++){
+    $factorial =1;
+    for ($i=$n;$i>1;$i--){
+        $factorial= $factorial * $i;
+        $j++;
+        
+    }
+    printf("Factorial number is %d = %d\n",$i,$factorial);
+}
+echo "$j total operation";
+//smart coding 1 to 10 factorial number
+
+$factorial = 1; 
+    for ($i = 1; $i <=10; $i++){ 
+      $factorial = $factorial * $i;
+      printf("Factorial number is %d = %d",$number,$factorial);
+    } 
+echo "\n";
+
 //shortcut factorial program
 $n=6;
 for($i=$n,$factorial=1;$i>1;$i--){

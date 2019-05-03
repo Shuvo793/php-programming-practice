03240d930bbe10980bd6622f491d81b3e9b7120b
Shuvo793 <cse.engr.shuvo@gmail.com>
2019-05-03 19:39:46 +0600

    ternary operator and nesting ternary operator

---
+++ ternary_operator_and_nested_ternary_operator.php
@@ -0,0 +1,29 @@
+<?php 
+ 
+ $n = 10;
+/*  if (12 === $n){
+     echo "twelve";
+ }elseif( 10 === $n){
+     echo "ten";
+ }else{
+     echo "no match";
+ }
+
+echo "\n"; */
+ 
+//replace in ternary operator
+ 
+$m = (12==$n) ? "twelve" : (10==$n) ? "ten" : "no match";
+
+echo $m;
+
+echo "\n";
+
+$b = 12===$n ? "twelve" : "no match";
+echo $b;
+echo "\n";
+
+//odd and even number use ternary operator
+
+$oddEven= $n % 2 === 0 ? "even" : "odd";
+echo $oddEven; 

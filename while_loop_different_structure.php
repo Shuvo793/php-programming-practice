fa0e22b4faea230f95539802d9fb91c0f35ebd23
Shuvo793 <cse.engr.shuvo@gmail.com>
2019-05-05 17:39:16 +0600

    while loop different structure

---
+++ while_loop_different_structure.php
@@ -0,0 +1,21 @@
+<?php
+
+// Local structure
+
+/* $i =0;
+while ($i<6){
+    echo $i."\n";
+    $i++;
+}
+ */
+
+
+//different structure
+
+$i=0;
+while($i++<6){ //increment er shathe condition aksathe decleare kora jae bt output amara different pabo
+    echo $i."\n";
+}
+
+
+

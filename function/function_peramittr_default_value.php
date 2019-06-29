2dc90ee2831050ac01957ff005b2f7177f88f747
meSuvo <cse.engr.shuvo@gmail.com>
2019-06-29 14:23:43 +0600

    function peramitter default value concept clear

---
+++ function/function_peramittr_default_value.php
@@ -0,0 +1,16 @@
+<?php
+
+/* function serve($food,$numberOfItem){
+    echo "{$numberOfItem} of {$food} have been serve";
+}
+$ft="coffe";
+$n=2;
+serve($ft,$n); */
+
+
+//main topic
+function serve($food="coffe",$numberOfItem="1 cup"){
+    echo "{$numberOfItem} of {$food} have been serve";
+}
+
+serve("salad","2 plats");
